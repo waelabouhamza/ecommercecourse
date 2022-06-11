fd89b9fb8d446edb2435eff51a0af5a1ab6924aa
waelabouhamza <97783930+waelabouhamza@users.noreply.github.com>
2022-06-11 23:13:00 +0300

    refresh

--- lib/view/screen/onboarding.dart
+++ lib/view/screen/onboarding.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:ecommercecourse/core/constant/color.dart';
 import 'package:ecommercecourse/data/datasource/static/static.dart';
 import 'package:flutter/material.dart';
