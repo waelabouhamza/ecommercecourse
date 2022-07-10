4fda7a563235577b4a23e23248f2b13f0b3001ec
waelabouhamza <97783930+waelabouhamza@users.noreply.github.com>
2022-07-10 16:22:13 +0300

    refresh

--- lib/core/functions/alertexitapp.dart
+++ lib/core/functions/alertexitapp.dart
@@ -1,23 +1,31 @@
 import 'dart:io';
 
+import 'package:ecommercecourse/core/constant/color.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 Future<bool> alertExitApp() {
   Get.defaultDialog(
       title: "تنبيه",
+      titleStyle: TextStyle(color: AppColor.primaryColor , fontWeight: FontWeight.bold),
       middleText: "هل تريد الخروج من التطبيق",
       actions: [
         ElevatedButton(
+            style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all(AppColor.primaryColor)),
             onPressed: () {
               exit(0);
             },
-            child: Text("Confirm")),
+            child: Text("تاكيد")),
         ElevatedButton(
+            style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all(AppColor.primaryColor)),
             onPressed: () {
               Get.back();
             },
-            child: Text("cancel"))
+            child: Text("الغاء"))
       ]);
   return Future.value(true);
 }
