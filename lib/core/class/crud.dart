2082aed9395802672e139c4063a5b4fbfa8d2a48
waelabouhamza <97783930+waelabouhamza@users.noreply.github.com>
2022-08-12 12:44:46 +0300

    crud request

---
+++ lib/core/class/crud.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+import 'package:dartz/dartz.dart';
+import 'package:ecommercecourse/core/class/statusrequest.dart';
+import 'package:ecommercecourse/core/functions/checkinternet.dart';
+import 'package:http/http.dart' as http;
+
+class Crud {
+  Future<Either<StatusRequest, Map>> postData(String linkurl, Map data) async {
+    try {
+      if (await checkInternet()) {
+        var response = await http.post(Uri.parse(linkurl), body: data);
+        if (response.statusCode == 200 || response.statusCode == 201) {
+          Map responsebody = jsonDecode(response.body);
+          return Right(responsebody);
+        } else {
+          return const Left(StatusRequest.serverfailure);
+        }
+      } else {
+        return const Left(StatusRequest.offlinefailure);
+      }
+    } catch (_) {
+      return const Left(StatusRequest.serverfailure);
+    }
+  }
+}
---
+++ lib/core/class/statusrequest.dart
@@ -0,0 +1,7 @@
+enum StatusRequest {
+  loading , 
+  success , 
+  failure , 
+  serverfailure , 
+  offlinefailure 
+}
