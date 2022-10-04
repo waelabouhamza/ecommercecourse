0c31d18ab6eec1d263160fe5baa2e108905b453d
waelabouhamza <97783930+waelabouhamza@users.noreply.github.com>
2022-10-04 13:36:11 +0300

    refresh

--- lib/view/screen/language.dart
+++ lib/view/screen/language.dart
@@ -10,7 +10,7 @@ class Language extends GetView<LocaleController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
+      body: Container(  
           padding: const EdgeInsets.all(15),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
